92839ddfbbadf2132f8fd1c2c1ea163f85cc1ab5
andreea gurzu <104028747+slayyyyyyy@users.noreply.github.com>
2022-10-30 19:05:41 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 #include<string>
 
+
 class Carte{
 private:
     std::string nume;
@@ -32,6 +33,67 @@ public:
         os<<Distribuitor.id_distribuitor<<" "<<Distribuitor.nume<<" "<<Distribuitor.adresa;
         return os;
     }
+};
+
+class Angajat{
+private:
+    std::string nume;
+    int id_angajat;
+    float salariu;
+public:
+    Angajat(const std::string& nume_, int id_angajat_,float salariu_) : nume{nume_}, id_angajat{id_angajat_}, salariu{salariu_}
+        {std::cout<<"constructor Angajat"<<'\n';};
+    Angajat(const Angajat& other) : nume{other.nume}, id_angajat{other.id_angajat}, salariu{other.salariu} {std::cout<<"Constructor de copiere Angajat\n";}
+    Angajat& operator=(const Angajat& other)
+    {
+        nume = other.nume;
+        id_angajat = other.id_angajat;
+        salariu = other.salariu;
+        return *this;
+    } //operator= + de adaugat constructor de copiere
+    ~Angajat() {}; //destructor
+    friend std::ostream& operator<<(std::ostream& os,const Angajat& Angajat)
+    {
+        os<<Angajat.nume<<" "<<Angajat.id_angajat<<" "<<Angajat.salariu;
+        return os;
+    }
+
+};
+
+class Fidelitate{
+private:
+    std::string nume;
+    std::string nr_tlf;
+    std::string email;
+    int reducere;
+public:
+    Fidelitate(const std::string& nume_, const std::string& nr_tlf_, const std::string email_, int reducere_) : nume{nume_}, nr_tlf{nr_tlf_}, email{email_}, reducere{reducere_}
+
+            {std::cout<<"constructor Fidelitate"<<'\n';};
+    friend std::ostream& operator<<(std::ostream& os, const Fidelitate& Fidelitate)
+    {
+        os<<Fidelitate.nume<<" "<<Fidelitate.nr_tlf<<" "<<Fidelitate.email<<" "<<Fidelitate.reducere;
+        return os;
+    }
+
+};
+
+class Librarie{
+    Carte crt;
+    Angajat ang;
+    Distribuitor dis;
+    Fidelitate fid;
+};
+
+int main()
+{
+    Carte C1{"1984","George Orwell","distopie",326,20};
+    Distribuitor D1{1,"Bookdepot","Calea Plevnei"};
+
+
+    std::cout<<C1<<" "<<D1;
+    return 0;
+}
 };
 
 class Angajat{
