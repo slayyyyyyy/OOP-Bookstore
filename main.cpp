266517ea4dbbc50689dae2a878303da7f2f35c8d
slayyyyyyy <andreea.gurzu02@gmail.com>
2023-01-29 20:38:57 +0200

    tema 2

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include "Carti_joc.h"
 #include "Enciclopedie.h"
 #include "Librarie.h"
+#include "Exceptii.h"
 
 int main()
 {
@@ -17,14 +18,25 @@ int main()
     std::vector<Distribuitor> dis;
     std::vector<Angajat> ang;
     std::vector<Fidelitate> fid;
-    crt.push_back(Carte("1984", "George Orwell","distopie",224, 20 ));
-    crt.push_back(Carte("Fratii Karamazov", "Fyodor Dostoievski","fictiune filozofica",780, 40 ));
-    crt.push_back(Carte("Fluturi", "Irina Binder","romance",328, 15 ));
-    crt.push_back(Carte("Ion", "Liviu Rebreanu","interbelic",268, 25 ));
-   // crt.push_back(Carte("Margareta","Petre Ispirescu", "balzacian", 104, 32));
+    try {
+        crt.push_back(Carte("1984", "George Orwell", "distopie", 224, 20));
+        crt.push_back(Carte("Fratii Karamazov", "Fyodor Dostoievski", "fictiune filozofica", 780, 40));
+        crt.push_back(Carte("Fluturi", "Irina Binder", "romance", 328, 15));
+        crt.push_back(Carte("Ion", "Liviu Rebreanu", "interbelic", 268, 25));
+        crt.push_back(Carte("Margareta", "Petre Ispirescu", "balzacian", 104, 32));
+    }
+    catch(eroare_carte &error)
+    {std::cout<<error.what()<<"\n";}
+
     dis.push_back(Distribuitor(1, "Bookdepot", "Calea Plevnei 45" ));
-    ang.push_back(Angajat("Cosmin Andrei",15 ,2345.60f, 4, 20));
-   // ang.push_back(Angajat("Badulescu Raluca",23 ,2465.70f, 12, 15 ));
+
+    try {
+        ang.push_back(Angajat("Cosmin Andrei", 15, 2345.60f, 4, 20));
+        ang.push_back(Angajat("Badulescu Raluca", 23, 2465.70f, 12, 15));
+    }
+    catch(eroare_angajat &error)
+    {std::cout<<error.what()<<"\n";}
+
     fid.push_back(Fidelitate("Trifoi Margareta", "0721456790", "trifoi@margareta.ro", 10, 4,{"1984", "George Orwell","distopie",224, 20}));
     fid.push_back(Fidelitate("Marginean Sara", "0753534210", "saratzika@yahoo.ro", 20, 7,{"Fratii Karamazov", "Fyodor Dostoievski","fictiune filozofica",780, 40}));
     fid[0].aplicare_red();
@@ -33,5 +45,8 @@ int main()
 
     Carti_desenat Cartedesenat1("Sa desenam", "Lydia Bubble", "pentru adulti", 30, 43);
     std::cout<<Cartedesenat1.getpret();
+
+
+
     return 0;
 }
